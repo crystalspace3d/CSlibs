1ffeb4c01f15cfd3fbcdd3821b7a86d2b57d013b
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2005-06-21 20:45:38 +0000

    res added support for GCC 3.4.4 on MinGW. res bumped the version to 012.

--- CSlibs/trunk/build-mingw.sh
+++ CSlibs/trunk/build-mingw.sh
@@ -2,6 +2,6 @@
 
 rm -rf temp/libcal3d/mingw-*
 
-for gccver in 3.2.3 3.3.3 3.4.2 ; do
+for gccver in 3.2.3 3.3.3 3.4.2 3.4.4 ; do
   CC="mingw32-gcc-${gccver}" CXX="mingw32-g++-${gccver}" ./build-for-gcc.sh "mingw-gcc-${gccver}" ;
 done
