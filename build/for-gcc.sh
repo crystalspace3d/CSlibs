00b4345042f7ff9acb1863f12d3825857af9b9e6
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2006-03-09 15:44:20 +0000

    res made the following changes to cs-win32libs:   - Renamed the cal3d and cegui libs to contain the VC version in the library     file name.   - Added Bullet.   - Added SpiderMonkey.   - Updated CEGUI to v0-4 CVS branch of 2006-03-08.   - Added debug versions for Cal3d and CEGUI, for both VC7 and VC8.

--- build/for-gcc.sh
+++ build/for-gcc.sh
@@ -5,3 +5,4 @@ platform_short=`echo ${platform} | sed "s/\(.*\)-.*-.*/\1/"`
 
 build/cal3d.sh ${platform} ${platform_short}
 build/cegui.sh ${platform} ${platform_short}
+build/bullet.sh ${platform} ${platform_short}
