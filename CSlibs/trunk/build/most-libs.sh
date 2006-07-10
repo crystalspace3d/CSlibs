6515c8f9e1183d2f7bb1e4980fcbd68dacb3237d
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2006-07-10 11:11:49 +0000

    Removed mikmod, as it is currently not used by CS.

--- CSlibs/trunk/build/most-libs.sh
+++ CSlibs/trunk/build/most-libs.sh
@@ -11,7 +11,6 @@ for lib in lib3ds libpng libjpeg libmng libfreetype libogg libvorbis ; do
   build/lib.sh ${platform} ${lib} 
 done
 build/lib.sh ${platform} libcaca --disable-imlib2 --disable-ncurses --disable-x11 --disable-slang
-build/lib.sh ${platform} libmikmod --disable-af --disable-alsa --disable-esd --disable-oss --disable-sam9407 --disable-ultra --disable-dl
 build/ode.sh ${platform}
 
 cp temp/${platform}/prefix/lib/*.a libs/ReleaseGCCOnly_static/${platform}/
