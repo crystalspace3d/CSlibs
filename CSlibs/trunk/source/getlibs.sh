3e0660fbe4edc5f76f1514d88bfcb0883ace1bf8
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2006-11-28 11:35:17 +0000

    - Updated libpng to 1.2.13.   - Updated CEGUI to 0.5.0.   - Fixed trac #189 (ODE dependency on msvcp71.dll).

--- CSlibs/trunk/source/getlibs.sh
+++ CSlibs/trunk/source/getlibs.sh
@@ -4,13 +4,13 @@ scripts/svn-get libcal3d http://svn.gna.org/svn/cal3d/tags/release-0_11_0/cal3d/
 scripts/svn-get libode https://svn.sourceforge.net/svnroot/opende/branches/0.7/
 scripts/cvs-get lib3ds :pserver:anonymous@lib3ds.cvs.sourceforge.net:/cvsroot/lib3ds \
   lib3ds
-scripts/svn-get libCEGUI https://svn.sourceforge.net/svnroot/crayzedsgui/cegui_mk2/tags/v0-5-0-RC2/
+scripts/svn-get libCEGUI https://svn.sourceforge.net/svnroot/crayzedsgui/cegui_mk2/branches/v0-5/
 # Would be nice to find some JS1.6 CVS tag.
 scripts/cvs-get libjs :pserver:anonymous@cvs-mirror.mozilla.org:/cvsroot \
   JSRef -r SPIDERMONKEY_1_6_0_BRANCH
 
-scripts/url-get libpng ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng-1.2.12.tar.bz2 \
-  libpng-1.2.12 bz2
+scripts/url-get libpng http://mesh.dl.sourceforge.net/sourceforge/libpng/libpng-1.2.13.tar.bz2 \
+  libpng-1.2.13 bz2
 scripts/url-get libz http://www.zlib.net/zlib-1.2.3.tar.bz2 \
   zlib-1.2.3 bz2
 scripts/url-get libmng http://mesh.dl.sourceforge.net/sourceforge/libmng/libmng-1.0.9.tar.gz \
