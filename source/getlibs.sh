458c2ed5f787ba222efdaee4e78bfa00f37ec424
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2007-05-14 15:53:10 +0000

    - Updated FreeType to 2.3.4.   - Updated libpng to 1.2.16.   - Excluded SpiderMonkey (since it's not used by main CS any more).   - Updated PCRE (used by CEGUI) to 7.1.

--- source/getlibs.sh
+++ source/getlibs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 scripts/svn-get libcal3d http://svn.gna.org/svn/cal3d/tags/release-0_11_0/cal3d/
-scripts/svn-get libode https://opende.svn.sourceforge.net/svnroot/opende/branches/0.8/
+scripts/svn-get libode https://opende.svn.sourceforge.net/svnroot/opende/tags/0.8/
 scripts/cvs-get lib3ds :pserver:anonymous@lib3ds.cvs.sourceforge.net:/cvsroot/lib3ds \
   lib3ds
 scripts/svn-get libCEGUI https://crayzedsgui.svn.sourceforge.net/svnroot/crayzedsgui/cegui_mk2/branches/v0-5/
@@ -9,8 +9,8 @@ scripts/svn-get libCEGUI https://crayzedsgui.svn.sourceforge.net/svnroot/crayzed
 scripts/cvs-get libjs :pserver:anonymous@cvs-mirror.mozilla.org:/cvsroot \
   JSRef -r SPIDERMONKEY_1_6_0_BRANCH
 
-scripts/url-get libpng http://mesh.dl.sourceforge.net/sourceforge/libpng/libpng-1.2.13.tar.bz2 \
-  libpng-1.2.13 bz2
+scripts/url-get libpng http://mesh.dl.sourceforge.net/sourceforge/libpng/libpng-1.2.16.tar.bz2 \
+  libpng-1.2.16 bz2
 scripts/url-get libz http://www.zlib.net/zlib-1.2.3.tar.bz2 \
   zlib-1.2.3 bz2
 scripts/url-get libmng http://mesh.dl.sourceforge.net/sourceforge/libmng/libmng-1.0.9.tar.gz \
@@ -19,11 +19,11 @@ scripts/url-get libogg http://downloads.xiph.org/releases/ogg/libogg-1.1.3.tar.g
   libogg-1.1.3 gz
 scripts/url-get libvorbis http://downloads.xiph.org/releases/vorbis/libvorbis-1.1.2.tar.gz \
   libvorbis-1.1.2 gz
-scripts/url-get libfreetype http://mesh.dl.sourceforge.net/sourceforge/freetype/freetype-2.3.2.tar.bz2 \
-  freetype-2.3.2 bz2
+scripts/url-get libfreetype http://mesh.dl.sourceforge.net/sourceforge/freetype/freetype-2.3.4.tar.bz2 \
+  freetype-2.3.4 bz2
 scripts/url-get libcaca http://sam.zoy.org/libcaca/libcaca-0.9.tar.bz2 \
   libcaca-0.9 bz2
 scripts/url-get libbullet "http://www.continuousphysics.com/ftp/pub/test/index.php?dir=physics/source/&file=bullet-1.9f.zip" \
   bullet-1.9f zip
-scripts/url-get libpcre ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-6.7.tar.bz2 \
-  pcre-6.7 bz2
+scripts/url-get libpcre ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-7.1.tar.bz2 \
+  pcre-7.1 bz2
