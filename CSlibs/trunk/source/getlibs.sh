919eeecd6191938dee56b38567c7daf68090e0bf
mgist <mgist@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2008-12-27 23:18:00 +0000

    - dir fix.

--- CSlibs/trunk/source/getlibs.sh
+++ CSlibs/trunk/source/getlibs.sh
@@ -20,7 +20,7 @@ scripts/url-get libmng http://mesh.dl.sourceforge.net/sourceforge/libmng/libmng-
 scripts/url-get libogg http://downloads.xiph.org/releases/ogg/libogg-1.1.3.tar.gz \
   libogg-1.1.3 gz
 scripts/url-get libspeex http://downloads.xiph.org/releases/speex/speex-1.2rc1.tar.gz \
-  libspeex-1.2rc1 gz
+  speex-1.2rc1 gz
 scripts/url-get libvorbis http://downloads.xiph.org/releases/vorbis/libvorbis-1.2.0.tar.gz \
   libvorbis-1.2.0 gz
 scripts/url-get libfreetype http://download.savannah.gnu.org/releases/freetype/freetype-2.3.7.tar.bz2 \
