01b381dd83ff44faa2bda622ba539610155435c6
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2009-02-01 16:43:39 +0000

    Remove caca from MinGW build

--- CSlibs/trunk/build/most-libs.sh
+++ CSlibs/trunk/build/most-libs.sh
@@ -16,7 +16,6 @@ build/lib.sh ${platform} libfreetype
 for lib in libogg libspeex libvorbis ; do
   build/lib.sh ${platform} ${lib} "install"
 done
-build/lib.sh ${platform} libcaca install --disable-imlib2 --disable-ncurses --disable-x11 --disable-slang --disable-gl
 build/ode.sh ${platform}
 
 rm temp/${platform}/prefix/lib/*.la
