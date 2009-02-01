667ac3b17c844aad6972f5b9add67087b0002bc6
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2009-02-01 16:44:58 +0000

    Some more WX build work

--- CSlibs/trunk/build/wx.sh
+++ CSlibs/trunk/build/wx.sh
@@ -15,6 +15,16 @@ cd temp/libwx/${platform}
 ../../../source/libwx/configure --prefix=${prefix} -C --with-opengl --without-subdirs --enable-unicode
 make install
 cd ../../..
-cp ${prefix}/lib/wxbase28u_gcc_custom.dll libs/ReleaseGCCOnly/${platform}
-cp ${prefix}/lib/wxmsw28u_core_gcc_custom.dll libs/ReleaseGCCOnly/${platform}
-cp ${prefix}/lib/wxmsw28u_gl_gcc_custom.dll libs/ReleaseGCCOnly/${platform}
+
+OUTPREFIX=libs/prefix-wx/${platform}
+mkdir -p ${OUTPREFIX}/lib
+cp ${prefix}/lib/wxbase28u_gcc_custom.dll ${OUTPREFIX}/lib
+cp ${prefix}/lib/libwx_baseu-2.8.dll.a ${OUTPREFIX}/lib
+cp ${prefix}/lib/wxmsw28u_core_gcc_custom.dll ${OUTPREFIX}/lib
+cp ${prefix}/lib/libwx_mswu_core-2.8.dll.a ${OUTPREFIX}/lib
+cp ${prefix}/lib/wxmsw28u_gl_gcc_custom.dll ${OUTPREFIX}/lib
+cp ${prefix}/lib/libwx_mswu_gl-2.8.dll.a ${OUTPREFIX}/lib
+cp -r ${prefix}/include ${OUTPREFIX}/
+mkdir -p ${OUTPREFIX}/lib/wx
+cp -r ${prefix}/lib/wx/include ${OUTPREFIX}/lib/wx
+cat ${prefix}/bin/wx-config | sed -e "s!${prefix}!/usr/local!g" > ${OUTPREFIX}/wx-config
