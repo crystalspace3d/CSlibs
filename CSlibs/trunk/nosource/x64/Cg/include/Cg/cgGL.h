6d68b8faef9d2930a82cd4bb1a9a9810666b7a7c
mgist <mgist@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2009-04-26 06:39:16 +0000

    - Update Cg to 2.2

--- CSlibs/trunk/nosource/x64/Cg/include/Cg/cgGL.h
+++ CSlibs/trunk/nosource/x64/Cg/include/Cg/cgGL.h
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (c) 2002-2008, NVIDIA Corporation.
+ * Copyright (c) 2002-2009, NVIDIA Corporation.
  * 
  *  
  * 
@@ -140,6 +140,7 @@ CGGL_API void CGGLENTRY cgGLDisableProfile(CGprofile profile);
 
 CGGL_API CGprofile CGGLENTRY cgGLGetLatestProfile(CGGLenum profile_type);
 CGGL_API void CGGLENTRY cgGLSetOptimalOptions(CGprofile profile);
+CGGL_API char const ** CGGLENTRY cgGLGetOptimalOptions(CGprofile profile);
 
 /******************************************************************************
  *** Program Managment Functions                                 
