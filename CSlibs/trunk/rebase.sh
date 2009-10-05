dc7ffdcd00617aa9b7ab0e936ec1eb09f3b11389
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2009-10-05 15:27:18 +0000

    Update for VC7/7.1 removal, WX libs, x64 libs

--- CSlibs/trunk/rebase.sh
+++ CSlibs/trunk/rebase.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #rebase_bin="/d/Programme/Microsoft SDKs/Windows/v1.0/Bin/ReBase"
-rebase_bin="/c/Programme/Microsoft Visual Studio .NET/Common7/Tools/Bin/ReBase"
+rebase_bin="/c/Programme/Microsoft Visual Studio 8/Common7/Tools/Bin/ReBase"
 
 rebase()
 {
@@ -14,15 +14,22 @@ startbase=0x70000000
 # Rebase common DLLs
 base=`rebase $startbase libs/release/*.dll libs/releasenocygwin/*.dll`
 # Rebase per-version DLLs, ignore debug versions
-rebase $base libs/ReleaseVC7Only/*-csvc7.dll > /dev/null
-rebase $base libs/ReleaseVC71Only/*-csvc71.dll > /dev/null
-rebase $base libs/ReleaseVC8Only/*-csvc8.dll > /dev/null
-rebase $base libs/ReleaseVC9Only/*-csvc9.dll > /dev/null
+rebase $base libs/ReleaseVC8Only/*-csvc8.dll libs/ReleaseWXVC8Only/*-csvc8.dll > /dev/null
+rebase $base libs/ReleaseVC9Only/*-csvc9.dll libs/ReleaseWXVC9Only/*-csvc9.dll > /dev/null
 # Rebase static DLLs; use startbase here as well, since the common libs are
 # (almost) not used anyway
-rebase $startbase libs/ReleaseVC7Only_static/*-csvc7.dll > /dev/null
-rebase $startbase libs/ReleaseVC71Only_static/*-csvc71.dll > /dev/null
 rebase $startbase libs/ReleaseVC8Only_static/*-csvc8.dll > /dev/null
 rebase $startbase libs/ReleaseVC9Only_static/*-csvc9.dll > /dev/null
+
+startbase64=0x170000000
+# Rebase common DLLs
+base=`rebase $startbase64 libs/release-x64/*.dll libs/releasenocygwin-x64/*.dll`
+# Rebase per-version DLLs, ignore debug versions
+rebase $base libs/ReleaseVC8Only-x64/*-csvc8-x64.dll libs/ReleaseWXVC8Only-x64/*-csvc8-x64.dll > /dev/null
+rebase $base libs/ReleaseVC9Only-x64/*-csvc9-x64.dll libs/ReleaseWXVC9Only-x64/*-csvc9-x64.dll > /dev/null
+# Rebase static DLLs; use startbase here as well, since the common libs are
+# (almost) not used anyway
+rebase $startbase64 libs/ReleaseVC8Only_static-x64/*-csvc8-x64.dll > /dev/null
+rebase $startbase64 libs/ReleaseVC9Only_static-x64/*-csvc9-x64.dll > /dev/null
 
 # MinGW DLLs don't need it since their linker automatically sets a base address
