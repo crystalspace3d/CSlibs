687a7920d234be0bed700791cefbaa17835b0340
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2009-12-27 19:00:13 +0000

    Bullet build script tweak

--- CSlibs/trunk/build/bullet.sh
+++ CSlibs/trunk/build/bullet.sh
@@ -12,7 +12,7 @@ fi
 
 prefix=$(pwd)/temp/libbullet/prefix-${platform}
 cd temp/libbullet/${platform}
-CC="${CC}.exe" CXX="${CXX}.exe" cmake -G "MSYS Makefiles" -DCMAKE_BUILD_TYPE=RelWithDebInfo "-DCMAKE_INSTALL_PREFIX=${prefix}" ../../../source/libbullet
+CC="${CC}.exe" CXX="${CXX}.exe" cmake -G "MSYS Makefiles" -DCMAKE_BUILD_TYPE=RelWithDebInfo "-DCMAKE_INSTALL_PREFIX=${prefix}" -DBUILD_EXTRAS=OFF -DBUILD_DEMOS=OFF ../../../source/libbullet
 cd src/LinearMath
 make install
 cd ../BulletCollision
