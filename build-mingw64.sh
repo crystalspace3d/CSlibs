f91f2ead4a00fa2470ffea84560c331c906ac059
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2010-05-02 02:56:41 +0000

    Build script tweaks

--- build-mingw64.sh
+++ build-mingw64.sh
@@ -4,8 +4,8 @@ export BUILD_TARGET=x86_64-w64-mingw32
 export CC=$BUILD_TARGET-gcc
 export CXX=$BUILD_TARGET-g++
 export RANLIB=$BUILD_TARGET-ranlib
-export CONFIGUREOPTS=--host=$BUILD_TARGET-mingw32 
-#build/most-libs.sh mingw64
+export CONFIGUREOPTS=--host=$BUILD_TARGET
+build/most-libs.sh mingw64
 
 # Build for a set of different GCC versions
 for gccver_full in 4.4.4 ; do
