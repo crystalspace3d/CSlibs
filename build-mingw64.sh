c4e93700be38e2e9ea7fcdc6e4315d37eae95506
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2010-08-29 15:01:35 +0000

    Updated gcc versions for mingw(64) builds. Build environment changes for mingw64.

--- build-mingw64.sh
+++ build-mingw64.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+MINGW64_PREFIX=/c/mingw64
+MINGW64_DEFAULT=$MINGW64_PREFIX-4.5
+
+OLDPATH="$PATH"
+
+export PATH="$MINGW64_DEFAULT/bin:$OLDPATH"
+
 export BUILD_TARGET=x86_64-w64-mingw32
 export CC=$BUILD_TARGET-gcc
 export CXX=$BUILD_TARGET-g++
@@ -8,7 +15,8 @@ export CONFIGUREOPTS=--host=$BUILD_TARGET
 build/most-libs.sh mingw64
 
 # Build for a set of different GCC versions
-for gccver_full in 4.4.4 ; do
+for gccver_full in 4.5.1 ; do
   gccver=`echo $gccver_full | sed "s/\([0-9]\?\)\.\([0-9]\?\)\.[0-9]\?/\1.\2/"`
+  export PATH="$MINGW64_PREFIX-${gccver}/bin:$OLDPATH"
   CC="x86_64-w64-mingw32-gcc-${gccver_full}" CXX="x86_64-w64-mingw32-g++-${gccver_full}" build/for-gcc.sh "mingw64-gcc-${gccver}" ;
 done
