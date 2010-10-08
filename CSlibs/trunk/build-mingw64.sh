a5d066c59986545a92859dbdc3bdbd95194b4446
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2010-10-08 19:07:32 +0000

    Update for new mingw64 gcc

--- CSlibs/trunk/build-mingw64.sh
+++ CSlibs/trunk/build-mingw64.sh
@@ -15,7 +15,7 @@ export CONFIGUREOPTS=--host=$BUILD_TARGET
 build/most-libs.sh mingw64
 
 # Build for a set of different GCC versions
-for gccver_full in 4.5.1 ; do
+for gccver_full in 4.5.2 ; do
   gccver=`echo $gccver_full | sed "s/\([0-9]\?\)\.\([0-9]\?\)\.[0-9]\?/\1.\2/"`
   export PATH="$MINGW64_PREFIX-${gccver}/bin:$OLDPATH"
   CC="x86_64-w64-mingw32-gcc-${gccver_full}" CXX="x86_64-w64-mingw32-g++-${gccver_full}" build/for-gcc.sh "mingw64-gcc-${gccver}" ;
