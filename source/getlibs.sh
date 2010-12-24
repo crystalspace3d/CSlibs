5634c29e44028a207a96170023de080219c3cf94
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2010-12-24 01:16:08 +0000

    - Updated libogg to 1.2.2.  - Updated libvorbis to 1.3.2.

--- source/getlibs.sh
+++ source/getlibs.sh
@@ -19,12 +19,12 @@ scripts/url-get libjpeg http://ijg.org/files/jpegsrc.v8b.tar.gz \
   jpeg-8b gz
 scripts/url-get libmng http://mesh.dl.sourceforge.net/sourceforge/libmng/libmng-1.0.10.tar.gz \
   libmng-1.0.10 gz
-scripts/url-get libogg http://downloads.xiph.org/releases/ogg/libogg-1.2.0.tar.gz \
-  libogg-1.2.0 gz
+scripts/url-get libogg http://downloads.xiph.org/releases/ogg/libogg-1.2.2.tar.gz \
+  libogg-1.2.2 gz
 scripts/url-get libspeex http://downloads.xiph.org/releases/speex/speex-1.2rc1.tar.gz \
   speex-1.2rc1 gz
-scripts/url-get libvorbis http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.1.tar.bz2 \
-  libvorbis-1.3.1 bz2
+scripts/url-get libvorbis http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.2.tar.bz2 \
+  libvorbis-1.3.2 bz2
 scripts/url-get libfreetype http://download.savannah.gnu.org/releases/freetype/freetype-2.4.4.tar.bz2 \
   freetype-2.4.4 bz2
 scripts/url-get libbullet http://bullet.googlecode.com/files/bullet-2.77.tgz \
