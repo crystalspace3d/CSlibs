62171484bda8127b8c66045ae5c454bff4610dbe
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2011-04-23 12:16:50 +0000

    Missing files

---
+++ CSlibs/branches/2.0/build/libpng.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+platform=$1
+
+SOURCES=$(pwd)/source/libpng
+mkdir -p temp/${platform}/libpng
+cd temp/${platform}/libpng
+CMAKE_OPTS=""
+CMAKE_OPTS="$CMAKE_OPTS -DCMAKE_BUILD_TYPE=RelWithDebInfo"
+CMAKE_OPTS="$CMAKE_OPTS -DCMAKE_INSTALL_PREFIX=$(pwd)/../prefix"
+CMAKE_OPTS="$CMAKE_OPTS -DBUILD_SHARED_LIBS=off"
+CMAKE_OPTS="$CMAKE_OPTS -DSKIP_INSTALL_FILES=on"
+CMAKE_OPTS="$CMAKE_OPTS -DPNG_SHARED=off"
+CMAKE_OPTS="$CMAKE_OPTS -DPNG_STATIC=on"
+if [ -n "$BUILD_TARGET" ] ; then
+  CMAKE_OPTS="$CMAKE_OPTS -DCMAKE_SYSTEM_NAME=Windows"
+fi
+CC="${CC-gcc}.exe" CXX="${CXX-g++}.exe" cmake -G "MSYS Makefiles" $CMAKE_OPTS $SOURCES
+make install
+mv $(pwd)/../prefix/lib/libpng15.a $(pwd)/../prefix/lib/libpng.a
