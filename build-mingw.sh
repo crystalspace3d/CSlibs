70023ade2d71fcfa65aae6ae03a587099a562ecc
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2011-04-23 11:03:00 +0000

    Update for compilers in latest mingw, mingw64

--- build-mingw.sh
+++ build-mingw.sh
@@ -6,7 +6,7 @@
 build/most-libs.sh mingw
 
 # Build for a set of different GCC versions
-for gccver_full in 3.4.5 4.4.0 4.5.0 ; do
+for gccver_full in 3.4.5 4.4.0 4.5.2 ; do
   gccver=`echo $gccver_full | sed "s/\([0-9]\?\)\.\([0-9]\?\)\.[0-9]\?/\1.\2/"`
   CC="mingw32-gcc-${gccver_full}" CXX="mingw32-g++-${gccver_full}" build/for-gcc.sh "mingw-gcc-${gccver}" ;
 done
