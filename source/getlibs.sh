734685a962c67ce8c8a0c66a71ad9ae39e778c2e
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2011-12-11 12:59:21 +0000

    - Updated libpng to 1.5.6.  - Updated libogg to 1.3.3.  - Updated freetype to 2.4.7.  - Updated bullet to 2.79.  - Updated pcre to 8.20.

--- source/getlibs.sh
+++ source/getlibs.sh
@@ -11,26 +11,26 @@ scripts/url-get pkg-config http://ftp.gnome.org/pub/gnome/binaries/win32/depende
   pkg-config-0.23 gz
 
 # Base libs
-scripts/url-get libpng ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng-1.5.2.tar.xz \
-  libpng-1.5.2 xz
+scripts/url-get libpng ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng-1.5.6.tar.xz \
+  libpng-1.5.6 xz
 scripts/url-get libz http://zlib.net/zlib-1.2.5.tar.bz2 \
   zlib-1.2.5 bz2
 scripts/url-get libjpeg http://ijg.org/files/jpegsrc.v8c.tar.gz \
   jpeg-8c gz
 scripts/url-get libmng http://mesh.dl.sourceforge.net/sourceforge/libmng/libmng-1.0.10.tar.gz \
   libmng-1.0.10 gz
-scripts/url-get libogg http://downloads.xiph.org/releases/ogg/libogg-1.2.2.tar.gz \
-  libogg-1.2.2 gz
+scripts/url-get libogg http://downloads.xiph.org/releases/ogg/libogg-1.3.0.tar.gz \
+  libogg-1.3.0 gz
 scripts/url-get libspeex http://downloads.xiph.org/releases/speex/speex-1.2rc1.tar.gz \
   speex-1.2rc1 gz
 scripts/url-get libvorbis http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.2.tar.bz2 \
   libvorbis-1.3.2 bz2
-scripts/url-get libfreetype http://download.savannah.gnu.org/releases/freetype/freetype-2.4.4.tar.bz2 \
-  freetype-2.4.4 bz2
-scripts/url-get libbullet http://bullet.googlecode.com/files/bullet-2.78-r2387.tgz \
-  bullet-2.78 gz
-scripts/url-get libpcre ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.12.tar.bz2 \
-  pcre-8.12 bz2
+scripts/url-get libfreetype http://download.savannah.gnu.org/releases/freetype/freetype-2.4.7.tar.bz2 \
+  freetype-2.4.7 bz2
+scripts/url-get libbullet http://bullet.googlecode.com/files/bullet-2.79-rev2440.tgz \
+  bullet-2.79 gz
+scripts/url-get libpcre ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.20.tar.bz2 \
+  pcre-8.20 bz2
 scripts/url-get lib3ds http://kent.dl.sourceforge.net/sourceforge/lib3ds/lib3ds-1.3.0.zip \
   lib3ds-1.3.0 zip
 scripts/url-get libode "http://downloads.sourceforge.net/project/opende/ODE/0.11.1/ode-0.11.1.zip?use_mirror=dfn" \
