485a7ae843861ea275a1002e18e1fbaaae5ed87e
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2011-12-26 14:10:00 +0000

    Merge changes from trunk. Bump version.

--- build/bullet.sh
+++ build/bullet.sh
@@ -18,10 +18,21 @@ CMAKE_OPTS="$CMAKE_OPTS -DCMAKE_INSTALL_PREFIX=${prefix}"
 CMAKE_OPTS="$CMAKE_OPTS -DUSE_GLUT=OFF"
 CMAKE_OPTS="$CMAKE_OPTS -DBUILD_EXTRAS=OFF -DBUILD_DEMOS=OFF"
 CMAKE_OPTS="$CMAKE_OPTS -DINSTALL_LIBS=ON"
+if [ -n "$RANLIB" ] ; then
+  CMAKE_OPTS="$CMAKE_OPTS -DCMAKE_RANLIB=$RANLIB"
+fi
+if [ -n "$AR" ] ; then
+  CMAKE_OPTS="$CMAKE_OPTS -DCMAKE_AR=$AR"
+fi
 if [ -n "$BUILD_TARGET" ] ; then
   CMAKE_OPTS="$CMAKE_OPTS -DCMAKE_SYSTEM_NAME=Windows"
 fi
 CC="${CC}.exe" CXX="${CXX}.exe" cmake -G "MSYS Makefiles" $CMAKE_OPTS ../../../source/libbullet
 make install
 cd ../../..
-cp -p ${prefix}/lib/*.a libs/ReleaseGCCOnly/${platform}
+# Copy libs, but strip _RelWithDebugInfo from name
+for lib in ${prefix}/lib/*.a; do
+  lib_name=`basename $lib`
+  dest_name=`echo $lib_name | sed -e s/_RelWithDebugInfo//`
+  cp -p -v $lib libs/ReleaseGCCOnly/${platform}/$dest_name
+done
