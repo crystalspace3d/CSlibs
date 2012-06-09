9c2a24c1cca999d0dee88d3addbcb25d71baa081
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2012-06-09 15:24:13 +0000

    Updated libjpeg to 8d.

--- CSlibs/trunk/source/getlibs.sh
+++ CSlibs/trunk/source/getlibs.sh
@@ -15,8 +15,8 @@ scripts/url-get libpng ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng-1.5
   libpng-1.5.10 xz
 scripts/url-get libz http://zlib.net/zlib-1.2.7.tar.bz2 \
   zlib-1.2.7 bz2
-scripts/url-get libjpeg http://ijg.org/files/jpegsrc.v8c.tar.gz \
-  jpeg-8c gz
+scripts/url-get libjpeg http://ijg.org/files/jpegsrc.v8d.tar.gz \
+  jpeg-8d gz
 scripts/url-get libmng http://mesh.dl.sourceforge.net/sourceforge/libmng/libmng-1.0.10.tar.gz \
   libmng-1.0.10 gz
 scripts/url-get libogg http://downloads.xiph.org/releases/ogg/libogg-1.3.0.tar.gz \
