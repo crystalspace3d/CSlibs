23d2ea40d9537aff2f7adfd8a649dcdf939abd12
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2012-10-18 13:32:41 +0000

    Tag 2.1_002 release

---
+++ CSlibs/tags/2.1_002/build/wx.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+TOP=`dirname $0`/
+
+platform=$1
+platform_short=$2
+
+if test ! -e temp/libwx/${platform} ; then
+	mkdir -p temp/libwx/${platform}
+fi
+if test ! -e libs/ReleaseGCCOnly/${platform} ; then
+	mkdir -p libs/ReleaseGCCOnly/${platform}
+fi
+
+prefix=$(pwd)/temp/libwx/prefix-${platform}
+cd temp/libwx/${platform}
+export CFLAGS="-I$(pwd)/../../${platform_short}/prefix/include"
+export CXXFLAGS="-I$(pwd)/../../${platform_short}/prefix/include"
+export LDFLAGS="-L$(pwd)/../../${platform_short}/prefix/lib"
+../../../source/libwx/configure --prefix=${prefix} $CONFIGUREOPTS -C --with-opengl --without-subdirs --enable-unicode --enable-vendor=cs${platform}
+make install
+cd ../../..
+
+OUTPREFIX=libs/prefix-wx/${platform}
+mkdir -p ${OUTPREFIX}/lib
+cp -pr ${prefix}/lib ${OUTPREFIX}/
+cp -pr ${prefix}/include ${OUTPREFIX}/
+cat ${prefix}/bin/wx-config | sed -e "s!\${prefix}/include!%CSLIBSPATH_MSYS%/${platform_short}/include!g" | sed -e "s!${prefix}!%CSLIBSPATH_MSYS%/${platform}!g" > ${OUTPREFIX}/wx-config-${platform}
+${TOP}/debug-extract.sh `ls -1 ${OUTPREFIX}/lib/*.dll`
---
+++ CSlibs/tags/2.1_002/build/zlib.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+platform=$1
+
+SOURCES=$(pwd)/source/libz
+rm -f $SOURCES/zconf.h
+mkdir -p temp/${platform}/libz
+cd temp/${platform}/libz
+CMAKE_OPTS=""
+CMAKE_OPTS="$CMAKE_OPTS -DCMAKE_BUILD_TYPE=RelWithDebInfo"
+CMAKE_OPTS="$CMAKE_OPTS -DCMAKE_INSTALL_PREFIX=$(pwd)/../prefix"
+CMAKE_OPTS="$CMAKE_OPTS -DBUILD_SHARED_LIBS=off"
+CMAKE_OPTS="$CMAKE_OPTS -DSKIP_INSTALL_FILES=on"
+if [ -n "$BUILD_TARGET" ] ; then
+  CMAKE_OPTS="$CMAKE_OPTS -DCMAKE_SYSTEM_NAME=Windows"
+fi
+CC="${CC}.exe" CXX="${CXX}.exe" cmake -G "MSYS Makefiles" $CMAKE_OPTS $SOURCES
+make install
+rm $(pwd)/../prefix/lib/libz.dll.a
