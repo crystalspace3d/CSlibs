946b21b82f56113af0b4ac1569bd05f9535cdb4e
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2013-02-08 18:01:09 +0000

    Update bullet.pc generator script to also replace @BULLET_DOUBLE_DEF@.

--- CSlibs/trunk/bullet_pc.sh
+++ CSlibs/trunk/bullet_pc.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 BULLET_VERSION=`cat source/libbullet/CMakeLists.txt | grep BULLET_VERSION | sed -e "s/.*(\(.*\))/\\1/" | sed -e "s/.* //"`
 echo "prefix=%CSLIBSPATH_WINE%/mingw" > libs/bullet.pc
-cat source/libbullet/bullet.pc.cmake | sed -e s/@BULLET_VERSION@/${BULLET_VERSION}/ | sed -e s/-L@LIB_DESTINATION@// | sed -e s+@INCLUDE_INSTALL_DIR@+\${prefix}/../common/include/bullet+ >> libs/bullet.pc
+cat source/libbullet/bullet.pc.cmake | sed -e s/@BULLET_VERSION@/${BULLET_VERSION}/ | sed -e s/-L@LIB_DESTINATION@// \
+  | sed -e s+@INCLUDE_INSTALL_DIR@+\${prefix}/../common/include/bullet+ | sed -e s/@BULLET_DOUBLE_DEF@// >> libs/bullet.pc
