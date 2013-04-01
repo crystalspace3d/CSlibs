5a8865f57bb9eb66d6acc8235711fb4141f2fa00
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2013-04-01 19:55:25 +0000

    More debuggable WX for MinGW

--- build/wx.sh
+++ build/wx.sh
@@ -14,10 +14,10 @@ fi
 
 prefix=$(pwd)/temp/libwx/prefix-${platform}
 cd temp/libwx/${platform}
-export CFLAGS="-I$(pwd)/../../${platform_short}/prefix/include"
-export CXXFLAGS="-I$(pwd)/../../${platform_short}/prefix/include"
+export CFLAGS="-I$(pwd)/../../${platform_short}/prefix/include -fno-omit-frame-pointer"
+export CXXFLAGS="-I$(pwd)/../../${platform_short}/prefix/include -fno-omit-frame-pointer"
 export LDFLAGS="-L$(pwd)/../../${platform_short}/prefix/lib"
-../../../source/libwx/configure --prefix=${prefix} $CONFIGUREOPTS -C --with-opengl --without-subdirs --enable-unicode --enable-vendor=cs${platform}
+../../../source/libwx/configure --prefix=${prefix} $CONFIGUREOPTS -C --with-opengl --without-subdirs --enable-unicode --enable-vendor=cs${platform} --enable-debug_info --enable-debug_gdb
 make ${MAKEOPTS} install
 cd ../../..
 
