00cfdc27b31d7f2ca271019d1c1f61fa972db34d
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2014-04-26 20:04:24 +0000

    Updated libjpeg to 9a.

--- CSlibs/trunk/build/debug-extract.sh
+++ CSlibs/trunk/build/debug-extract.sh
@@ -7,7 +7,15 @@ fi
 
 for dll in $* ; do
   echo Separating debug info from ${dll}
+  # How it's supposed to work:
   $objcopy --only-keep-debug ${dll} ${dll}.dbg
   $objcopy --strip-unneeded ${dll}
   $objcopy --long-section-names=enable --add-gnu-debuglink=${dll}.dbg ${dll}
+  # What needs to be done according to http://sourceware.org/bugzilla/show_bug.cgi?id=14527#c4 :
+  #objcopy --only-keep-debug ${dll} ${dll}.dbg
+  #objcopy --long-section-names=enable --add-gnu-debuglink="${dll}.dbg" ${dll}
+  #objcopy --only-keep-debug ${dll} ${dll}.dbg
+  #objcopy --long-section-names=enable --remove-section=.gnu_debuglink ${dll}
+  #objcopy --long-section-names=enable --add-gnu-debuglink="${dll}.dbg" ${dll}
+  #objcopy --strip-unneeded ${dll}
 done
