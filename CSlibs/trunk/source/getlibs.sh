e124494801f6b85810f272e172247599530d7f87
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2014-04-26 22:18:11 +0000

    Updated libvorbis to 1.3.4

--- CSlibs/trunk/source/getlibs.sh
+++ CSlibs/trunk/source/getlibs.sh
@@ -23,8 +23,8 @@ scripts/url-get libogg http://downloads.xiph.org/releases/ogg/libogg-1.3.1.tar.x
   libogg-1.3.1 xz
 scripts/url-get libspeex http://downloads.xiph.org/releases/speex/speex-1.2rc1.tar.gz \
   speex-1.2rc1 gz
-scripts/url-get libvorbis http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.3.tar.xz \
-  libvorbis-1.3.3 xz
+scripts/url-get libvorbis http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.4.tar.xz \
+  libvorbis-1.3.4 xz
 scripts/url-get libfreetype http://download.savannah.gnu.org/releases/freetype/freetype-2.4.11.tar.bz2 \
   freetype-2.4.11 bz2
 scripts/url-get libbullet http://bullet.googlecode.com/files/bullet-2.80-rev2531.tgz \
